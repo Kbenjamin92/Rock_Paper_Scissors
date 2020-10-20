df53d576c28f8270deadd4110c2d09ed07831f7f
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2020-10-20 12:10:15 -0400

    updated the lists on the DOM

--- game.js
+++ game.js
@@ -20,10 +20,13 @@ document.querySelector(".computer-score").append(computer);
 
 // Restart the game option which returns the score to 0
 const restartGame = document.querySelector(".restart").addEventListener("click", () => {
-    // player score
+// restart player score
     player.textContent = score.playerScore = 0;
-// computer score
+// restart computer score
     computer.textContent = score.computerScore = 0; 
+
+    let playerList = document.querySelector("#list-player-choice")
+    playerList.removeChild(rock);
     
 });
 // The game is best of 3 tries add that functionality, use a for loop to perfrom this action
@@ -55,47 +58,45 @@ let computersChoice = options[Math.floor(Math.random() * options.length)];
 
 // rock button, generates random computer logic
 const rock = document.querySelector(".player-rock").addEventListener("click", () => {
-    let rock = document.createElement("p");
+    let rock = document.createElement("li");
     rock.textContent = "Rock"
-    document.querySelector(".player-score").append(rock);
+    document.querySelector("#list-player-choice").append(rock);
 
-    let computersSelection = document.createElement("p");
+    let computersSelection = document.createElement("li");
     computersSelection.textContent = computersChoice
-    document.querySelector(".computer-score").append(computersSelection);
+    document.querySelector("#list-computer-choice").append(computersSelection);
 
     game(rock, null, null);
 
 });
 // papers button, generates random computer logic
 const paper =  document.querySelector(".player-paper").addEventListener("click", () => {
-    let paper = document.createElement("p");
+    let paper = document.createElement("li");
     paper.textContent = "Paper"
-    document.querySelector(".player-score").append(paper);
+    document.querySelector("#list-player-choice").append(paper);
 
-    let computersSelection = document.createElement("p");
+    let computersSelection = document.createElement("li");
     computersSelection.textContent = computersChoice
-    document.querySelector(".computer-score").append(computersSelection);
+    document.querySelector("#list-computer-choice").append(computersSelection);
 
     game(null, paper, null);
-
-
 });
 // scissors button, generates random computer logic
 const scissors = document.querySelector(".player-scissors").addEventListener("click", () => {
-    let scissors = document.createElement("p");
+    let scissors = document.createElement("li");
     scissors.textContent = "Scissor"
-    document.querySelector(".player-score").append(scissors);
+    document.querySelector("#list-player-choice").append(scissors);
 
-    let computersSelection = document.createElement("p");
+    let computersSelection = document.createElement("li");
     computersSelection.textContent = computersChoice
-    document.querySelector(".computer-score").append(computersSelection);
+    document.querySelector("#list-computer-choice").append(computersSelection);
 
     game(null, null, scissors);
 
 });
 
 const game = (rock, paper, scissors) => {
-
+    // functionality for the game
     if(computersChoice === options[0] && paper) {
         player.textContent = score.playerScore++ + 1;
 
@@ -115,7 +116,7 @@ const game = (rock, paper, scissors) => {
         computer.textContent = score.computerScore++ + 1
     }
     
-    // functionality for duplicate choses 
+    // functionality for duplicate choices
     if (computersChoice === options[0] && rock) {
         let rockDraw = document.createElement("h1");
         rockDraw.textContent = "Draw"
@@ -131,7 +132,5 @@ const game = (rock, paper, scissors) => {
         scissorsDraw.textContent = "Draw"
         document.querySelector(".score").append(scissorsDraw);
     }
-
-    
 }
 

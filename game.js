658a769e865731532b915a4d8efa30f019354518
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2020-10-27 15:23:29 -0400

    added animation to the game

--- game.js
+++ game.js
@@ -37,15 +37,34 @@ const restartGame = document.querySelector(".restart").addEventListener("click",
     document.querySelector(".players-choice-paper").style.visibility = "hidden"
     document.querySelector(".players-choice-scissors").style.visibility = "hidden"
 
-
-
 });
+
+//load animations
+gsap.from(".player-rock", {duration: 1.3, opacity: 0,
+y: () => Math.random() * 400 - 200, ease: "back"});
+//animation paper button
+gsap.from(".player-paper", {duration: 1.3, opacity: 0,
+y: () => Math.random() * 400 - 200, ease: "back"});
+//animation scissor button
+gsap.from(".player-scissors", {duration: 1.3, opacity: 0,
+y: () => Math.random() * 400 - 200, ease: "back"});
+//animation for the computer score
+gsap.from(".computers-container", {duration: 2,  x: -90, ease: "back"})
+//animation for the players score
+gsap.from(".players-container", {duration: 2,  x: 90, ease: "back"})
+// animation for new game button 
+gsap.from(".restart", {duration: 2,  x: 90, ease: "elastic"})
+
+
+
 //checks the score for the winner
 const checkScore = () => {
     if (score.computerScore >= 3) {
         document.querySelector(".computer-Winner").style.visibility = "visible"
+        gsap.from(".computer-Winner", {duration: 1,  x: 90, ease: "bounce"})
     } else if (score.playerScore >= 3) {
         document.querySelector(".player-Winner").style.visibility = "visible"
+        gsap.from(".player-Winner", {duration: 1,  x: 90, ease: "bounce"})
     }
 }
 
@@ -53,21 +72,19 @@ const checkScore = () => {
 TODO LIST 
 
 */ 
-// document.querySelector(".player-Winner").style.visibility = "visible"
-// document.querySelector(".computer-Winner").style.visibility = "visible"
-
-// document.querySelector(".rock").style.visibility = "visible"
-// document.querySelector(".paper").style.visibility = "visible"
-// document.querySelector(".scissors").style.visibility = "visible"
   
 const rockFunc = () => {
     document.querySelector(".rock").style.visibility = "visible"
+    gsap.from(".rock", {duration: 2,  x: 90, ease: "bounce"})
+
 }
 const paperFunc = () => {
     document.querySelector(".paper").style.visibility = "visible"
+    gsap.from(".paper", {duration: 2,  x: 90, ease: "bounce"})
 }
 const scissorsFunc = () => {
     document.querySelector(".scissors").style.visibility = "visible"
+    gsap.from(".scissors", {duration: 2,  x: 90, ease: "bounce"})
 }
 
 //computers results
@@ -80,6 +97,16 @@ const options = [computerSelectionRock, computerSelectionPaper, computerSelectio
 
 //logic to run the code after the rock paper scissors queue.
 const runRockLogic = (event) => {
+    //animation for the results
+    gsap.from(".computers-choice-rock", {duration: 1.5,  x: 80, ease: "back"});
+    gsap.from(".computers-choice-paper", {duration: 1.5,  x: 80, ease: "back"});
+    gsap.from(".computers-choice-scissors", {duration: 1.5,  x: 80, ease: "back"});
+    // animation for player resuls
+    gsap.from(".players-choice-rock", {duration: 1.5,  x: -20, ease: "back"});
+    gsap.from(".players-choice-paper", {duration: 1.5,  x: -20, ease: "back"});
+    gsap.from(".players-choice-scissors", {duration: 1.5,  x: -20, ease: "back"});
+
+
     document.querySelector(".players-choice-rock").style.visibility = "visible"
     document.querySelector(".players-choice-paper").style.visibility = "hidden"
     document.querySelector(".players-choice-scissors").style.visibility = "hidden"
@@ -103,6 +130,16 @@ const runRockLogic = (event) => {
 }
 //run paper logic
 const runPaperLogic = (event) => {
+    //animation for computer results
+    gsap.from(".computers-choice-rock", {duration: 1.5,  x: 80, ease: "back"});
+    gsap.from(".computers-choice-paper", {duration: 1.5,  x: 80, ease: "back"});
+    gsap.from(".computers-choice-scissors", {duration: 1.5,  x: 80, ease: "back"});
+    // animation for player results
+    gsap.from(".players-choice-rock", {duration: 1.5,  x: -20, ease: "back"});
+    gsap.from(".players-choice-paper", {duration: 1.5,  x: -20, ease: "back"});
+    gsap.from(".players-choice-scissors", {duration: .5 , x: -20, ease: "back"});
+
+
     document.querySelector(".players-choice-paper").style.visibility = "visible"
     document.querySelector(".players-choice-rock").style.visibility = "hidden"
     document.querySelector(".players-choice-scissors").style.visibility = "hidden"
@@ -125,6 +162,15 @@ const runPaperLogic = (event) => {
 }
 //run scissors logic
 const runScissorsLogic = (event) => {
+    //animation for computer result
+    gsap.from(".computers-choice-rock", {duration: 1.5,  x: 80, ease: "back"});
+    gsap.from(".computers-choice-paper", {duration: 1.5,  x: 80, ease: "back"});
+    gsap.from(".computers-choice-scissors", {duration: 1.5,  x: 80, ease: "back"});
+    //animation for player results
+    gsap.from(".players-choice-rock", {duration: 1.5,  x: -20, ease: "back"});
+    gsap.from(".players-choice-paper", {duration: 1.5,  x: -20, ease: "back"});
+    gsap.from(".players-choice-scissors", {duration: 1.5,  x: -20, ease: "back"});
+
     document.querySelector(".players-choice-scissors").style.visibility = "visible"
     document.querySelector(".players-choice-paper").style.visibility = "hidden"
     document.querySelector(".players-choice-rock").style.visibility = "hidden"
@@ -198,12 +244,15 @@ const game = (rock, paper, scissors, computerOption) => {
      // functionality for duplicate choices
     if (computerOption === options[0] && rock) {
         document.querySelector(".draw").style.visibility = "visible"
+        gsap.from(".draw", {duration: 1,  x: 90, ease: "bounce"})
 
     } else if (computerOption === options[1] && paper) {
         document.querySelector(".draw").style.visibility = "visible"
+        gsap.from(".draw", {duration: 1,  x: 90, ease: "bounce"})
 
     } else if (computerOption === options[2] && scissors) {
         document.querySelector(".draw").style.visibility = "visible"
+        gsap.from(".draw", {duration: 1,  x: 90, ease: "bounce"})
     } 
 
     // functionality for the game
